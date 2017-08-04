3ff657ad3dd4fc190224584d2b8d34f656901793
tuposacp <tuposacp@gmail.com>
2017-08-04 21:10:54 +1000

    commit

--- sourceCode/public_html/index.php
+++ sourceCode/public_html/index.php
@@ -1,155 +1,247 @@
 <?php
-session_start();
-$user = null;
-if(isset($_SESSION[activeuser])){
-  $login=1;
-}
-else{
-  $login=0;
-}
+include 'login.php';
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 ?>
-
-
 <!DOCTYPE>
-<html>
+<html lang="en">
 <head>
 
-  <!--Meta tag for android view-->
-  <meta name="viewport" content="initial-scale=1">
-  <meta name="HandheldFriendly" content="true" />
-  <meta name="viewport" content="user-scalable=no, width=device-width, maximum-scale=5.0" />
-  <meta name="apple-mobile-web-app-capable" content="yes" />
   <meta charset="utf-8">
-  <link rel="stylesheet" type="text/css" href="data/css/style.css">
+  <meta name = "viewport" content = "user-scalable = yes, width = device-width, maximum-scale = 1, initial-scale = 1" />
+  <meta name = "apple-mobile-web-app-capable" content = "yes" />
+  <meta name = "description" content = "Book trading platform for students, book-lovers and bookworms. Trading books are in physical form, not an E-Book.">
+  <meta name = "keywords" content = "Book, trade, book trade, Physical Books, Buy Books, Sell Books, Trade Books">
+
+  <link rel = "stylesheet" type = "text/css" href = "data/css/style.css">
   <title>Book.trade</title>
-  <meta name="description" content="Book trading platform for students, book-lovers and bookworms. Trading books are in physical form, not an E-Book.">
-  <meta name="keywords" content="Book, trade, book trade, Physical Books, Buy Books, Sell Books, Trade Books">
 </head>
-<body onload='location.href="#home"'>
+<body>
 
-    <!-- Fixed Top Bar -->
-    <div id="topBarWrap" class="topBarWrap">
-      <div id="topBar" class="topBar">
+  <!--Fixed Top Bar-->
+  <div id="topBar" class="topBar">
+    <a id="title" href="#home">Book.trade</a>
+
+    <ul>
+      <li><a href="#libraryContent">Library</a></li>
+      <li><a href="#">Books</a>
+
         <ul>
-          <!--<td><h1><a id="title" href="#home"><img src="data/images/WLogo.png" width="40%" height="40%"></a></h1></td>-->
-          <li><h1><a id="title" href="#home">Book.trade</a></h1></li>
-          <li><h3><a href="#libraryContent">Library</a></h3></li>
-          <li><h3><a href="#listContent">Booklist</a></h3></li>
-          <?php if($login==0):?>
-            <li><h3><a href="#registerContent">Register</a></h3></li>
-            <li><h3><a href="#loginContent">Login</a></h3></li>
-          <?php else:?>
-            <li><h3><a><?php echo $_SESSION["activeuser"]?></a></h3></li>
-            <li><h3><a href="#logoutContent">Logout</a></h3></li>
-          <?php endif;?>
+          <li><a href="#bookList">Book List</a></li>
+          <li><a href="#">Books</a>
+
+            <ul>
+              <li><a href="#bookList">Book List</a></li>
+              <li><a href="#newBooks">New Books</a></li>
+            </ul>
+
+          </li>
+          <li><a href="#newBooks">New Books</a></li>
         </ul>
-      </div>
+
+      </li>
+      <li><a>Account</a>
+
+        <ul>
+          <li><a href="#registerContent">Register</a></li>
+          <li><a href="#loginContent">Login</a></li>
+        </ul>
+
+      </li>
+      <li><a>user</a>
+
+        <ul>
+          <li><a href="#logoutContent">Logout</a></li>
+        </ul>
+
+      </li>
+    </ul>
+
+  </div>
+
+
+  <!--Scrollable Page-->
+  <div id="content" class="content">
+
+    <!--Welcome Page-->
+    <div id="home" class="home">
+      <p>Welcome!</p><br>
     </div>
 
 
-    <!--Scrollable Page-->
-    <!--Left Bar-->
-    <div id="leftBar" class="leftBar">
+    <!--Adding New Books-->
+    <div id="newBooks" class="newBooks">
+      <form id="newBooks" class="newBooks" method="post">
+
+        <table>
+          <tr>
+            <td align="right">Book Name:</td>
+            <td align="left"><input id="newbookname"type="text" name="newbookname" /></td>
+          </tr>
+
+          <tr>
+            <td align="right">Book Auther:</td>
+            <td align="left"> <input id="newbookauther"type="text" name="newbookauther" /></td>
+          </tr>
+
+          <tr>
+            <td align="right">ISBN:</td>
+            <td align="left"><input id="newbookisbn" type="text" name="newbookosbn" /></td>
+          </tr>
+
+          <tr>
+            <td align="right">Book Condition:</td>
+            <td align="left"><input id="bookcondition" type="text" name="bookcondition" /></td>
+          </tr>
+
+          <tr>
+            <td align="right"></td>
+            <td align="left"><input type="submit" name="addbook" value="Add Book" /></td>
+          </tr>
+        </table>
+
+      </form>
     </div>
 
-    <!--Middle Content-->
-    <div id="content" class="content">
-      <!--Welcome Page-->
-      <div id="home" class="home">
-        <p>Welcome <?php echo $_SESSION["activeuser"]?> !</p>
-      </div>
 
-      <!--Login-->
-      <div id="loginContent" class="loginContent">
-        <form method="post">
-            <?php include 'login.php';?>
-            <h2>Login</h2>
-            <?php if(!$success==1): ?>
-              <p align="center" style="color:darkred;">Invalid credentials!</p>
-            <?php endif; ?>
-            <table>
-              <tr>
-                <td align="right">Username:</td>
-                <td align="left"><input id="username"type="text" name="username" /></td>
-              </tr>
-              <tr>
-                <td align="right">Password:</td>
-                <td align="left"><input id="password" type="password" name="password" /></td>
-              </tr>
-              <tr>
-                <td align="right"></td>
-                <td align="left"><input type="submit" name="submit" value="Submit" /></td>
-              </tr>
-            </table>
-          </form>
-      </div>
 
-      <!--register content-->
-      <div id="registerContent" class="registerContent" >
-          <?php include 'register.php';?>
-          <form method="post">
-            <h2>Register</h2>
-            <?php if($formValid==false): ?>
-              <p align="center"style="color:darkred;">All fields required!</p>
-            <?php elseif($usernameValid==false): ?>
-              <p align="center" style="color:darkred;">
-                Username must be between 4 to 12 characters!<br>
-                Username should not start with a number nor with a space!
-              </p>
-            <?php elseif($passwordMissMatch == true): ?>
-              <p align="center" style="color:darkred;">
-                Password did not match!
-              </p>
-            <?php elseif($passwordValid == false): ?>
-              <p align="center" style="color:darkred;">
-                Password invalid!<br>
-                Password must be alphanumeric containing at least one symbol!<br>
-                Password must be between 6 to 12 characters!
-              </p>
-            <?php elseif($existName==1): ?>
-              <p align="center" style="color:darkred;">
-                Given username already exists!<br>
-              </p>
-            <?php elseif($existEmail==1): ?>
-              <p align="center" style="color:darkred;">
-                Given email already exists!<br>
-              </p>
-            <?php elseif($registration==1): ?>
-              <p align="center" style="color:darkgreen;">
-                Regisrtation successful!!!<br>
-              </p>
-            <?php endif; ?>
-            <table>
-              <tr>
-                <td align="right">Username:</td>  <td align="left"><input id="usernamesignup"type="text" name="usernamesignup" /></td>
-              </tr>
-              <tr>
-                <td align="right">Email:</td>    <td align="left"> <input id="emailsignup"type="text" name="emailsignup" /></td>
-              </tr>
-              <tr>
-                <td align="right">Password:</td>  <td align="left"><input id="passwordsignup" type="password" name="passwordsignup" /></td>
-              </tr>
-              <tr>
-                <td align="right">Confirm Password:</td>  <td align="left"><input id="passwordsignup_confirm" type="password" name="passwordsignup_confirm" /></td>
-              </tr>
-              <tr>
-                <td align="right"></td><td align="left"><input type="submit" name="submit" value="Register!" /></td>
-              </tr>
-            </table>
-          </form>
-        </div>
+    <!--Login-->
+    <div id="loginContent" class="loginContent">
+      <form method="post">
+        <h2>Login</h2>
+        <p class = "warning">Invalid credentials!</p>
 
-      <!--Logout-->
-      <div id="logoutContent" class="logoutContent">
-          <?php include 'logout.php';?>
-          <form method="post">
-            <p>Do you really wanna logout??</p>
-            <input type="submit" name="logoutYes" value="Yes!" />
-            <input type="submit" name="logoutNo" value="No I wanna Stay!" />
-          </form>
-        </div>
+        <table>
+          <tr>
+            <td align="right">Username:</td>
+            <td align="left"><input id="username"type="text" name="username" /></td>
+          </tr>
 
-      <!--Booklist Content-->
+          <tr>
+            <td align="right">Password:</td>
+            <td align="left"><input id="password" type="password" name="password" /></td>
+          </tr>
+
+          <tr>
+            <td align="right"></td>
+            <td align="left"><input type="submit" name="submit" value="Submit" /></td>
+          </tr>
+        </table>
+
+      </form>
     </div>
+
+
+
+
+    <!--register content-->
+    <div id="registerContent" class="registerContent" >
+      <form method="post">
+        <h2>Register</h2>
+
+        <p class = "warning">All fields required!</p>
+        <p class = "warning">
+          Username must be between 4 to 12 characters!<br>
+          Username should not start with a number nor with a space!
+        </p>
+        <p class = "warning">
+          Password did not match!
+        </p>
+        <p class = "warning">
+          Password invalid!<br>
+          Password must be alphanumeric containing at least one symbol!<br>
+          Password must be between 6 to 12 characters!
+        </p>
+        <p class = "warning">
+          Given username already exists!<br>
+        </p>
+        <p class = "warning">
+          Given email already exists!<br>
+        </p>
+        <p style="color:darkgreen;">
+          Regisrtation successful!!!<br>
+        </p>
+
+        <table>
+          <tr>
+            <td align="right">Username:</td>
+            <td align="left"><input id="usernamesignup"type="text" name="usernamesignup" /></td>
+          </tr>
+
+          <tr>
+            <td align="right">Email:</td>
+            <td align="left"> <input id="emailsignup"type="text" name="emailsignup" /></td>
+          </tr>
+
+          <tr>
+            <td align="right">Password:</td>
+            <td align="left"><input id="passwordsignup" type="password" name="passwordsignup" /></td>
+          </tr>
+
+          <tr>
+            <td align="right">Confirm Password:</td>
+            <td align="left"><input id="passwordsignup_confirm" type="password" name="passwordsignup_confirm" /></td>
+          </tr>
+
+          <tr>
+            <td align="right"></td>
+            <td align="left"><input type="submit" name="submit" value="Register!" /></td>
+          </tr>
+        </table>
+
+      </form>
+    </div>
+
+
+
+    <!--Logout-->
+    <div id="logoutContent" class="logoutContent">
+
+      <form method="post">
+        <p>Do you really wanna logout??</p>
+        <input type="submit" name="logoutYes" value="Yes!" />
+        <input type="submit" name="logoutNo" value="No I wanna Stay!" />
+      </form>
+
+    </div>
+
+    <!--Current Books-->
+    <div id="bookList" class="bookList">
+      <form method="post" id="bookList" class="bookList">
+
+        <table>
+          <tr>
+            <td align="right">List Name:</td>
+            <td align="left"><input id="newlistname"type="text" name="newlistname" /></td>
+          </tr>
+
+          <tr>
+            <td align="right"></td>
+            <td align="left"> <input id="emailsignup" type="text" name="emailsignup" /></td>
+          </tr>
+
+          <tr>
+            <td align="right">Password:</td>
+            <td align="left"><input id="passwordsignup" type="password" name="passwordsignup" /></td>
+          </tr>
+
+          <tr>
+            <td align="right">Confirm Password:</td>
+            <td align="left"><input id="passwordsignup_confirm" type="password" name="passwordsignup_confirm" /></td>
+          </tr>
+
+          <tr>
+            <td align="right"></td>
+            <td align="left"><input type="submit" name="submit" value="Register!" /></td>
+          </tr>
+        </table>
+
+      </form>
+    </div>
+
+  </div>
 </body>
 </html>
--- sourceCode/public_html/login.php
+++ sourceCode/public_html/login.php
@@ -1,38 +1,20 @@
-  <?php
-  session_start();
-  $success=1;
-  // Same as error_reporting(E_ALL);
-  ini_set('error_reporting', E_ALL);
-  // Same as error_reporting(E_ALL);
-  ini_set('display_errors', 1);
-  $connection = mysqli_connect('localhost', 'root', 'Godonly1');
-  if (!$connection){
-      die("Database Connection Failed" . mysqli_error($connection));
+<?php
+
+if( isset($_POST['username'])
+and isset($_POST['password'])
+and !is_null($_POST['username'])
+and !is_null($_POST['password'])) {
+
+  try {
+    $connectionToDatabase = new PDO('mysql: host=localhost; dbname=booktrade',
+    'root',
+    'Godonly1',
+    array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION) );
+  }
+  catch (PDOException $e) {
+    echo 'Could not connect to database';
   }
 
-  $select_db = mysqli_select_db($connection, 'booktrade');
-  if (!$select_db){
-      die("Database Selection Failed" . mysqli_error($connection));
-  }
+}
 
-    //3. If the form is submitted or not.
-    //3.1 If the form is submitted
-    if (isset($_POST['username']) and isset($_POST['password'])){
-    //3.1.1 Assigning posted values to variables.
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    //3.1.2 Checking the values are existing in the database or not
-    $query = "SELECT * FROM `user` WHERE userName='$username' and userPassword='$password'";
-
-    $result = mysqli_query($connection, $query) or die(mysqli_error($connection));
-    $count = mysqli_num_rows($result);
-    //3.1.2 If the posted values are equal to the database values, then session will be created for the user.
-    if ($count == 1){
-      $_SESSION["activeuser"]=$username;
-      header('location: /#home');
-    }elseif ($count ==0) {
-      $success = 0;
-    }
-    }
-
-  ?>
+?>
